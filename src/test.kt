86c199e029930b343b4f47efee7133e05dcf07e9
kuromasu <basmanovaelena64@gmail.com>
2026-03-28 23:24:20 +0700

    Сделали функцию посмотреть параметры и класс персон

--- src/test.kt
+++ src/test.kt
@@ -1,3 +1,46 @@
 fun main() {
-    print("Првиет мир, я заебался")
+    println("Првиет мир, я заебался")
+
+    var gg = Person("gg")
+
+
+    gg.showParameter()
 }
+
+class Person() {
+
+    var name: String = "Нпс"
+    var infection: Boolean = false
+    var profession: String = "Безработный"
+    var hobby: String = "Отсутствует"
+    var health: String = "Здоровый"
+    var luggage: String = "Отсутствует"
+    var alive: Boolean = true
+
+    constructor(_name: String) : this() {
+        name = _name
+    }
+
+
+    /*Эта функция показывает параметры персонажа.
+    Чтобы вызвать написать: имя объекта.showParameter()*/
+    fun showParameter(){
+        println("Имя: $name")
+        if("$infection" == "true"){
+            println("Заражение: да")
+        } else {
+            println("Заражение: нет")
+        }
+        println("Профессия: $profession")
+        println("Хобби: $hobby")
+        println("Здоровье: $health")
+        println("Багаж: $luggage")
+        if("$alive" == "true"){
+            println("Жив: да")
+        } else {
+            println("Жив: нет")
+        }
+
+    }
+}
+
